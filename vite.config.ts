f221535cb163f88902851add04c73a97b395ae3f
Nasir Uddin Shamim <shamim@Nasirs-MacBook-Air.local>
2026-09-19 07:35:59 +0600

    ci: deploy web app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.GITHUB_ACTIONS ? "/ayat-answers/" : "/",
   server: {
     host: "::",
     port: 8080,
